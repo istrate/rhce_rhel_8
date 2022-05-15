e75644a14893a6155a4166e6dcd0dfe0e60d0664
lpwoodhouse <lpwoodhouse@gmail.com>
2022-05-16 08:59:01 +0900

    done

--- practice_exams/lisenet_sample_exam/plays/adhoc.sh
+++ practice_exams/lisenet_sample_exam/plays/adhoc.sh
@@ -5,10 +5,10 @@ read -sp 'root password?: ' pwd
 ansible all -u root -e "ansible_password=$pwd" -m user \
 -a "name=automation state=present shell=/bin/bash"
 
-ansible all -u root -e "ansible_password=4pwd" -m lineinfile \
+ansible all -u root -e "ansible_password=$pwd" -m lineinfile \
 -a "path=/etc/sudoers.d/automation create=true line='automation ALL=(ALL) NOPASSWD: ALL'"
 
-ansible all -u root -e "ansible_password=4pwd" -m authorized_key \
+ansible all -u root -e "ansible_password=$pwd" -m authorized_key \
 -a "user=automation state=present key={{ lookup('file', '/home/automation/.ssh/id_rsa.pub') }}"
 
 ansible all -u automation -m ping
